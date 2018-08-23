7989d3266e677346eb2c0006a0823eb018f5bd1e
baklavacado <benny.l.wu@gmail.com>
2018-08-22 21:49:46 -0700

    implement redis cache and update artillery yml to replicate more accurate site usage

--- server/server-pg.js
+++ server/server-pg.js
@@ -2,40 +2,93 @@ const nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// need to create a file to select data
-const db = require('../db/queries-pg.js');
-
+const redis = require('redis');
+const responseTime = require('response-time');
 
 const app = express();
+
+// postgres queries
+const db = require('../db/queries-pg.js');
+
+// create and connect redis client to local instance
+const reClient = redis.createClient();
+
+// print redis errors to console
+reClient.on('error', (err) => {
+  console.log('Error', err);
+});
+
+// use response-time as middleware
+app.use(responseTime());
 
 // to parse our data and use req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+// GET 
+
 app.get('/api/about/hosts/:id', (req, res) => {
   // console.log(req.params);
-  db.selectHostInfo(req.params.id, (err, result) => {
-    if (err) {
-      console.log('error in server pg', err);
-      res.status(500).send(err);
+  const id = req.params.id;
+  reClient.get(id, (error, result) => {
+    if (result) {
+      res.send(result);
     } else {
-      res.status(200).send(result);
+      db.selectHostInfo(id, (err, result) => {
+        if (err) {
+          console.log('error in server pg', err);
+          res.status(500).send(err);
+        } else {
+          reClient.setex(id, 180, JSON.stringify(result));
+          res.status(200).send(result);
+        }
+      });
     }
   });
 });
 
 app.get('/api/about/reviews/:userId', (req, res) => {
   // console.log('test', req.params);
-  db.reviewsForHost(req.params.userId, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err);
+  const userId = req.params.userId;
+  reClient.get(userId, (err, result) => {
+    if (result) {
+      res.json(result);
     } else {
-      res.status(200).json(result);
+      db.reviewsForHost(userId, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        } else {
+          reClient.setex(userId, 180, JSON.stringify(result));
+          res.status(200).json(result);
+        }
+      });
     }
   });
 });
+
+app.get('/api/about/neighborhood/:listingId', (req, res) => {
+  const listingId = req.params.listingId;
+  reClient.get(listingId, (err, result) => {
+    if (result) {
+      res.json(result);
+    } else {
+      db.neighborhoodInfo(+req.params.listingId, (err, result) => {
+        if (err) {
+          console.log('err from s-pg', err);
+          res.status(500).send(err);
+        } else {
+          reClient.setex(listingId, 180, JSON.stringify(result));
+          res.status(200).json(result);
+        }
+      });
+    }
+  });
+});
+
+// POST
 
 app.post('/api/about/reviews/new', (req, res) => {
   // console.log('from server post reviews', req.body);
@@ -51,6 +104,20 @@ app.post('/api/about/reviews/new', (req, res) => {
   });
 });
 
+
+// PUT
+app.put('/api/about/reviews/:listingId/update', (req, res) => {
+  db.updateReviewRating(req.body, (err, result) => {
+    if (err) {
+      console.log('err from server update review', err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+// DELETE
 app.delete('/api/about/reviews/:listingId/delete', (req, res) => {
   // console.log('test', req.params);
   db.deleteReviewForHost(+req.params.listingId, (err, result) => {
@@ -64,28 +131,7 @@ app.delete('/api/about/reviews/:listingId/delete', (req, res) => {
   });
 });
 
-app.put('/api/about/reviews/:listingId/update', (req, res) => {
-  db.updateReviewRating(req.body, (err, result) => {
-    if (err) {
-      console.log('err from server update review', err);
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(result);
-    }
-  });
-});
-
-app.get('/api/about/neighborhood/:listingId', (req, res) => {
-  db.neighborhoodInfo(+req.params.listingId, (err, result) => {
-    if (err) {
-      console.log('err from s-pg', err);
-      res.status(500).send(err);
-    } else {
-      res.status(200).json(result);
-    }
-  });
-});
-
+//CONNECTION
 app.listen(3001, () => {
   console.log('Server started on 3001');
 });
